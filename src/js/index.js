e663e617c9ee6453297b736d3f0ca5f6d80b6d79
Luca Bacchi <bacchilu@gmail.com>
2022-05-30 16:58:30 +0200

    new part

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ const Counter = function () {
     };
 
     return (
-        <div className="card m-4">
+        <div className="card text-bg-light m-4">
             <div className="card-body">
                 <h5 className="card-title">Counter</h5>
                 <form className="row row-cols-lg-auto m-4">
@@ -61,32 +61,34 @@ const TempConv = function () {
     };
 
     return (
-        <div className="card m-4">
+        <div className="card text-bg-light m-4">
             <div className="card-body">
                 <h5 className="card-title">Temperature Converter</h5>
                 <form className="row row-cols-lg-auto m-4">
                     <div className="col-auto">
-                        <div className="input-group">
+                        <div className="form-floating">
                             <input
                                 className="form-control"
                                 type="number"
                                 step="0.01"
                                 value={celsius}
                                 onChange={onCelcius}
+                                placeholder="Celcius"
                             />
-                            <span className="input-group-text">Celsius</span>
+                            <label>Celcius</label>
                         </div>
                     </div>
                     <div className="col-auto">
-                        <div className="input-group">
+                        <div className="form-floating">
                             <input
                                 className="form-control"
                                 type="number"
                                 step="0.01"
                                 value={fahrenheit}
                                 onChange={onFahrenheit}
+                                placeholder="Fahrenheit"
                             />
-                            <span className="input-group-text">Fahrenheit</span>
+                            <label>Fahrenheit</label>
                         </div>
                     </div>
                 </form>
@@ -102,11 +104,95 @@ const TempConv = function () {
     );
 };
 
+const FlightBooker = function () {
+    const today = `${new Date().toISOString()}`.split('T')[0];
+
+    const [type, setType] = React.useState('SINGLE');
+    const [startDate, setStartDate] = React.useState(today);
+    const [endDate, setEndDate] = React.useState(today);
+
+    const onChange = function (e) {
+        if (e.target.name === 'type') setType(e.target.value);
+        if (e.target.name === 'start_date') setStartDate(e.target.value);
+        if (e.target.name === 'end_date') setEndDate(e.target.value);
+    };
+
+    const onSubmit = function (e) {
+        e.preventDefault();
+    };
+
+    const isDateValid = function (v) {
+        const d = new Date(v);
+        return !isNaN(d);
+    };
+
+    return (
+        <div className="card text-bg-light m-4">
+            <div className="card-body">
+                <h5 className="card-title">Flight Booker</h5>
+                <form className="row row-cols-lg-auto m-4" onSubmit={onSubmit}>
+                    <div className="col-auto">
+                        <div className="form-floating">
+                            <select className="form-select" name="type" value={type} onChange={onChange}>
+                                <option value="SINGLE">one-way flight</option>
+                                <option value="RETURN">return flight</option>
+                            </select>
+                            <label>Single or Return</label>
+                        </div>
+                    </div>
+                    <div className="col-auto">
+                        <div className="form-floating">
+                            <input
+                                className={`form-control ${isDateValid(startDate) ? '' : 'is-invalid'}`}
+                                type="date"
+                                placeholder="Start Date"
+                                name="start_date"
+                                value={startDate}
+                                onChange={onChange}
+                            />
+                            <label>Start Date</label>
+                        </div>
+                    </div>
+                    <div className="col-auto">
+                        <div className="form-floating">
+                            <input
+                                className={`form-control ${
+                                    type !== 'SINGLE' && !isDateValid(endDate) ? 'is-invalid' : ''
+                                }`}
+                                type="date"
+                                placeholder="Return Date"
+                                name="end_date"
+                                value={endDate}
+                                onChange={onChange}
+                                disabled={type === 'SINGLE'}
+                            />
+                            <label>Return Date</label>
+                        </div>
+                    </div>
+                    <div className="col-auto">
+                        <button type="submit" className="btn btn-primary mb-3" style={{marginTop: '8px'}}>
+                            Confirm identity
+                        </button>
+                    </div>
+                </form>
+                <p className="card-text">
+                    This is the{' '}
+                    <a href="https://eugenkiss.github.io/7guis/tasks/#flight" target="_blank">
+                        Flight Booker
+                    </a>
+                    . Nothing particular here: this is a classic React test.
+                </p>
+            </div>
+        </div>
+    );
+};
+
 const App = function () {
     return (
         <>
             <Counter />
             <TempConv />
+            <FlightBooker />
         </>
     );
 };
