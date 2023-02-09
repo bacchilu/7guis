eac2729a460070f7f9b1bae4a95ee1bb1fd32c49
Luca Bacchi <bacchilu@gmail.com>
2023-02-09 17:19:45 +0100

    Working with the CRUD app

--- src/js/crud.js
+++ src/js/crud.js
@@ -1,21 +1,39 @@
 import React from 'react';
 
 export const Crud = function () {
-    const [filter, setFilter] = React.useState('');
-    const [list, setList] = React.useState([
+    const [db, setDb] = React.useState([
         {id: 1, name: 'Hans', surname: 'Emil', selected: true},
         {id: 2, name: 'Max', surname: 'Mustermann', selected: false},
         {id: 3, name: 'Roman', surname: 'Tisch', selected: false},
     ]);
+    const [filter, setFilter] = React.useState('');
+    const [currentList, setCurrentList] = React.useState(db);
+    const [insertForm, setInsertForm] = React.useState({name: '', surname: ''});
+    React.useEffect(() => {
+        setCurrentList(
+            db.filter((item) => item.surname.toLocaleLowerCase().startsWith(filter.trim().toLocaleLowerCase()))
+        );
+    }, [db, filter]);
 
     const onFilter = function (e) {
-        setFilter(e.target.value);
+        const data = e.target.value;
+        setFilter(data);
     };
 
-    const listItems = list.map(function (item) {
+    const onChangeInsertForm = function (e) {
+        setInsertForm({...insertForm, [e.target.name]: e.target.value});
+    };
+
+    const onCreate = function () {
+        const [name, surname] = [insertForm.name.trim(), insertForm.surname.trim()];
+        if (db.find((item) => item.name === name && item.surname === surname) !== undefined) return;
+        setDb([...db, {name, surname, id: db.length + 1, selected: false}]);
+    };
+
+    const listItems = currentList.map((item) => {
         const onClick = function () {
-            setList(
-                list.map(function (e) {
+            setCurrentList(
+                currentList.map(function (e) {
                     return {...e, selected: item.id === e.id};
                 })
             );
@@ -60,20 +78,30 @@ export const Crud = function () {
                             <div className="row mb-3">
                                 <label className="col-sm-3 col-form-label">Name:</label>
                                 <div className="col-sm-9">
-                                    <input className="form-control" />
+                                    <input
+                                        className="form-control"
+                                        name="name"
+                                        value={insertForm.name}
+                                        onChange={onChangeInsertForm}
+                                    />
                                 </div>
                             </div>
                             <div className="row mb-3">
                                 <label className="col-sm-3 col-form-label">Surname:</label>
                                 <div className="col-sm-9">
-                                    <input className="form-control" />
+                                    <input
+                                        className="form-control"
+                                        name="surname"
+                                        value={insertForm.surname}
+                                        onChange={onChangeInsertForm}
+                                    />
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div className="row mb-3">
                         <div className="col">
-                            <button type="button" className="btn btn-primary me-3">
+                            <button type="button" className="btn btn-primary me-3" onClick={onCreate}>
                                 Create
                             </button>
                             <button type="button" className="btn btn-warning me-3">
