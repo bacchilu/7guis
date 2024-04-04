390e640c7bdf99cf17a1c86a1152bfa2067b1652
Luca Bacchi <bacchilu@gmail.com>
2024-04-04 17:03:37 +0200

    Using bootstrap via npm

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
 import {createRoot} from 'react-dom/client';
 
@@ -9,13 +11,14 @@ import {Timer} from './timer';
 
 const App = function () {
     return (
-        <>
+        <div className="container">
+            <h1>7GUIs</h1>
             <Counter />
             <TempConv />
             <FlightBooker />
             <Timer />
             <Crud />
-        </>
+        </div>
     );
 };
 
